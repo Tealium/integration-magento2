0b98c28c071c38edbfcacf9117b243a3878c8e94
VishJ <vish@onebricktech.com>
2020-10-30 10:02:00 +1000

    Fix grouped product add to cart issue

--- CustomerData/JsAddToCart.php
+++ CustomerData/JsAddToCart.php
@@ -38,34 +38,48 @@ class JsAddToCart implements SectionSourceInterface
         $result = [];
 	
         if ($product_id) {
-            $result = ['data'=>$this->_productHelper->getProductData($product_id)];
-			
-			//$data = $this->getCartDataCustom();
-			$items = $this->_cart->getQuote()->getAllVisibleItems();
-			$objectManager =  \Magento\Framework\App\ObjectManager::getInstance();  
-			$mediaUrl = $objectManager->get('Magento\Store\Model\StoreManagerInterface')
-            ->getStore()
-            ->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
-			
-			foreach($items as $item) {
-				$CartId[] = 	$item->getProductId();
-				$CartSku[] = 	$item->getSku();
-				$CartQty[] = 	$item->getQty();
-				$CartPrice[] =	number_format($item->getPrice(), 2, '.', '');
-				
-				$productCat = $objectManager->create('Magento\Catalog\Model\Product')->load($item->getProductId());
-				$productImage[] = $mediaUrl.$productCat->getImage();
-			}
-		
-			
-			$result['data']['cart_product_id'] = $CartId;
-			$result['data']['cart_product_price'] = $CartPrice;
-			$result['data']['cart_product_quantity'] = $CartQty;
-			$result['data']['cart_product_sku'] = $CartSku;
-			$result['data']['product_image_url'] = $productImage;
-            $result['data']['product_quantity'] = [(string)$qty];
-            $result['data']['product_id'] = [(string)$product_id];
+            if(is_array($product_id)) {
+                foreach($product_id as $pid) {
+                    $result['data'][$pid] = $this->_productHelper->getProductData($pid);
+                }
+            }
+            else {
+                $result = ['data'=> $this->_productHelper->getProductData($product_id)];        
+            }
+
+            $items = $this->_cart->getQuote()->getAllVisibleItems();
+            $objectManager =  \Magento\Framework\App\ObjectManager::getInstance();  
+            $mediaUrl = $objectManager->get('Magento\Store\Model\StoreManagerInterface')
+                        ->getStore()
+                        ->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
+
+            foreach($items as $item) {
+                $CartId[] = 	$item->getProductId();
+                $CartSku[] = 	$item->getSku();
+                $CartQty[] = 	$item->getQty();
+                $CartPrice[] =	number_format($item->getPrice(), 2, '.', '');
+                
+                $productCat = $objectManager->create('Magento\Catalog\Model\Product')->load($item->getProductId());
+                $productImage[] = $mediaUrl.$productCat->getImage();
+            }
+                    
+            $result['data']['cart_product_id'] = $CartId;
+            $result['data']['cart_product_price'] = $CartPrice;
+            $result['data']['cart_product_quantity'] = $CartQty;
+            $result['data']['cart_product_sku'] = $CartSku;
+            $result['data']['product_image_url'] = $productImage;
+
+            if(is_array($product_id)) {
+                $result['data']['product_quantity'] = $qty;
+                $result['data']['product_id'] = $product_id;
+            }
+            else {
+                $result['data']['product_quantity'] = [(string)$qty];
+                $result['data']['product_id'] = [(string)$product_id];
+            }
+
             $result['data']['tealium_event'] = 'cart_add';
+
         }
 
         return $result;
--- Observer/AddProduct.php
+++ Observer/AddProduct.php
@@ -57,18 +57,20 @@ class AddProduct implements ObserverInterface
         if (isset($requestParamList['qty'])) {
             $product_quantity = $requestParamList['qty'];
         }
+        $logger = \Magento\Framework\App\ObjectManager::getInstance()->get(\Psr\Log\LoggerInterface::class);
 
-        if (isset($requestParamList['super_group'])) {
-            $product_id = [];
+        if (isset($requestParamList['super_group'])) {        
             $product_quantity = [];
-            foreach ($requestParamList['super_group'] as $pid => $qty) {                
+            $product_id = [];
+            foreach ($requestParamList['super_group'] as $pid => $qty) {                              
+                $logger->info($pid." -> ".$qty);
                 if (intval($qty) > 0) {
                     $product_quantity[] = $qty;
-                    $product_id[] = $pid;
+                    $product_id[] = $pid;                    
                 }
             }
         }
-        
+
         $this->_customerSession->setTealiumAddProductId($product_id);
         $this->_customerSession->setTealiumAddProductQty($product_quantity);
         
